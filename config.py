faf07f7c3c072bed2b79c2d217c5a04963809019
businessaicardstudio-debug <business.aicardstudio@gmail.com>
2026-03-19 01:17:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = "8606980605:AAEeYDTXIz2nAtmy7lqnFaHC55_AHiKDc6c"
-ADMIN_IDS = [6383511175, 7869544426]  # Yours + friend
+ADMIN_IDS = [7869544426, 6383511175]
 API_ID = 37759576
 API_HASH = "127d61a5a5d335c22a7448b79e319808"
 PROXY_SOURCES = [
@@ -7,13 +7,8 @@ PROXY_SOURCES = [
     "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
     "https://www.proxy-list.download/api/v1/get?type=http"
 ]
-SHOP_LIST = [  # 2026 fresh leak shops - add more
-    "https://shein.com",
-    "https://boohoo.com",
-    "https://asos.com",
-    "https://nike.com",
-    "https://argos.co.uk",
-    "https://prettylittlething.com",
-    # +15 more from YouTube "shop sk dump 2026"
+SHOP_LIST = [
+    "https://www.shein.com", "https://www.boohoo.com", "https://www.asos.com",
+    "https://www.nike.com", "https://www.argos.co.uk", "https://www.prettylittlething.com",
+    "https://www.zaful.com", "https://www.forever21.com"
 ]
-
